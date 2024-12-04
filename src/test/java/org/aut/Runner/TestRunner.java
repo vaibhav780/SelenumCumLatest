45cc442f8c002cddf2fdb1f52bd7efb860c4292f
vaibhav870 <vaibhavcoding@gmail.com>
2024-12-04 20:34:24 +0530

    Uploading Project Selenium Latest Maven Parallel Run SauceLabs

--- src/test/java/org/aut/Runner/TestRunner.java
+++ src/test/java/org/aut/Runner/TestRunner.java
@@ -1,9 +1,8 @@
 package org.aut.Runner;
 
-import io.cucumber.junit.Cucumber;
+
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import org.junit.runner.RunWith;
 import org.testng.annotations.DataProvider;
 
 
